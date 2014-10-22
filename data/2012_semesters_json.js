f88a12c765704e176cd694e092de22502aa13774
Sean Lafferty <sixers2329@gmail.com>
2014-10-22 16:50:08 +0000

    Week 2 pie chart added

---
+++ data/2012_semesters_json.js
@@ -0,0 +1,44 @@
+[
+    {
+        "id": 1,
+        "course": "ACCT207",
+		"semdata": [
+				{"term": "2008 Fall", "freq": 35},
+				{"term": "2009 Spring", "freq": 20},
+				{"term": "2009 Fall", "freq": 225},
+				{"term": "2010 Spring", "freq": 16},
+				{"term": "2010 Fall", "freq": 6},
+				{"term": "2011 Spring", "freq": 1},
+				{"term": "2011 Fall", "freq": 30},
+				{"term": "2012 Spring", "freq": 0}
+				]
+    },
+    {
+        "id": 2,
+        "course": "ACCT208",
+		"semdata": [
+				{"term": "2008 Fall", "freq": 6},
+				{"term": "2009 Spring", "freq": 30},
+				{"term": "2009 Fall", "freq": 22},
+				{"term": "2010 Spring", "freq": 198},
+				{"term": "2010 Fall", "freq": 14},
+				{"term": "2011 Spring", "freq": 6},
+				{"term": "2011 Fall", "freq": 1},
+				{"term": "2012 Spring", "freq": 0}
+				]
+    },
+	{
+        "id": 3,
+        "course": "ACCT302",
+			"semdata": [
+				{"term": "2008 Fall", "freq": 0},
+				{"term": "2009 Spring", "freq": 1},
+				{"term": "2009 Fall", "freq": 7},
+				{"term": "2010 Spring", "freq": 11},
+				{"term": "2010 Fall", "freq": 22},
+				{"term": "2011 Spring", "freq": 90},
+				{"term": "2011 Fall", "freq": 47},
+				{"term": "2012 Spring", "freq": 0}
+				]
+    }
+]
